00cd2f51d72de7cfa969c76b197851961139671f
tyleo <tyler.wolf.leonhardt@gmail.com>
2024-07-28 06:26:38 -0700

    Window with inspector now renders

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use bevy::{app::PluginGroupBuilder, prelude::*};
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+fn make_window_plugin() -> WindowPlugin {
+    let primary_window = Window {
+        title: "Mine Sweeper!".into(),
+        resolution: (700., 800.).into(),
+        ..default()
+    };
+
+    WindowPlugin {
+        primary_window: Some(primary_window),
+        ..default()
+    }
+}
+
+fn make_default_plugins() -> PluginGroupBuilder {
+    let window_plugin = make_window_plugin();
+    DefaultPlugins.set(window_plugin)
+}
+
+fn startup_camera_system(mut commands: Commands) {
+    let camera_2d_bundle = Camera2dBundle {
+        camera: Camera { ..default() },
+        ..default()
+    };
+
+    commands.spawn(camera_2d_bundle);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins(make_default_plugins());
+
+    #[cfg(feature = "debug")]
+    app.add_plugins(WorldInspectorPlugin::new());
+
+    app.add_systems(Startup, startup_camera_system);
+
+    app.run();
 }
